c034a8dab188306dc9cc067c59c147dd613bf80d
tirthachetry <tirthachetri12@gmail.com>
2025-05-22 19:41:46 +0530

    Labour Management System --  Added jwt

--- src/main/java/com/labour/config/SecurityConfig.java
+++ src/main/java/com/labour/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.labour.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +18,7 @@ public class SecurityConfig{
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login").permitAll()
